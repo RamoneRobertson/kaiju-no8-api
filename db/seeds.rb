c4027755762e4913986dded322b2af73b8193694
Ramone Robertson <ramonerobertson10@gmail.com>
2024-12-24 14:34:51 +0900

    scraped episode titles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "nokogiri"
+require "open-uri"
+
+url = 'https://www.imdb.com/title/tt21975436/episodes/'
+html_content = URI.open(url, 'Accept-Language' => 'en', "User-Agent" => "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0").read
+doc = Nokogiri::HTML(html_content)
+
+titles = doc.search('h4 a .ipc-title__text').map do |element|
+  full_title = element.text.strip
+  full_title.sub(/^S\d+\.\E\d+ ∙ /, '')
+end
+
+titles.each { |title| puts title }
+
+
+
+# OR (if you need to iterate over multiple elements)
+
+# doc.search('.REPLACE_ME').each do |element|
+# p element.text.strip
+# end
