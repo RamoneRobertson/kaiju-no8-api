6a1be375857ec982c8e68d05819ed0f0d44a6c2d
Ramone Robertson <ramonerobertson10@gmail.com>
2024-12-28 19:34:17 +0900

    Finished filter by episode_num logic

---
+++ app/controllers/api/v1/episodes_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::EpisodesController < ApplicationController
+  def index
+    if params[:episode_num]
+      @episodes = Episode.where(episode_num: [ params[:episode_num].to_i ])
+    else
+      @episodes = Episode.all
+    end
+    render json: @episodes.order(release_date: :asc)
+  end
+
+  def create
+    @episode = Episode.new(episode_params)
+
+    if @episode.save
+      render json: @episode, status:
+    else
+      render json: { error: @episode.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def episode_params
+    params.require(:episode).permit(:title, :release_date, :description, :rating, :picture, :episode_num, :season)
+  end
+end
